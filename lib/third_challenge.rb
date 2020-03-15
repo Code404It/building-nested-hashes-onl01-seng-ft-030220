b2a9aee311e81193a1854af8d4a278d2144ab789
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-15 10:19:29 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,13 +1,1 @@
 def third_challenge
- 
-
-  epic_tragedy = {
-   :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-@@ -13,5 +13,5 @@ def third_challenge
-      :heroine => {name: "Juliette", age: "15", status: "alive"},
-      :heroine_friends => []
-   }
-}
-  }
- end
